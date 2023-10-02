57af9e991b6edaa0221fad36d67512744a698e5d
rrtxt <rizfiferdian@gmail.com>
2023-10-02 11:25:02 +0700

    Add Journal Submission Routing

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "reviewer.apps.ReviewerConfig"
 ]
 
 MIDDLEWARE = [
@@ -115,7 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+# settings.py
+
+# settings.py
+
+STATIC_URL = '/static/'
+
+# Additional directories to search for static files
+
+# Additional directories to search for static files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
