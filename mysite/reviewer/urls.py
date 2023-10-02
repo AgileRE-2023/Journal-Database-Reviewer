82dad57a3d11194b791ee0dcc5d7dde557bd16a0
rrtxt <rizfiferdian@gmail.com>
2023-10-02 13:02:35 +0700

    Add File Upload and Recommend View

--- mysite/reviewer/urls.py
+++ mysite/reviewer/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 #Tambah URL Route disini
+app_name = "reviewer"
+
 urlpatterns = [
-    path("", view=views.index, name='index')
+    path("", view=views.submission, name='submission'),
+    path("upload", view=views.upload, name='upload'),
+    path("recommend", view=views.recommend, name='recommend')
 ]
--- mysite/reviewer/views.py
+++ mysite/reviewer/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def submission(request):
     return render(request, "reviewer/index.html")
+
+def upload(request):
+    return render(request, 'reviewer/file_uploader.html')
+
+def recommend(request):
+    return render(request, 'reviewer/recommended_reviewers.html')
