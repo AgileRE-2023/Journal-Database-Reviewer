6912483f5b24611a7aea21fa2a7e1e4d16e83068
rrtxt <rizfiferdian@gmail.com>
2023-10-03 09:31:19 +0700

    Change Installed Apps

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "reviewer.apps.ReviewerConfig"
+    "reviewer"
 ]
 
 MIDDLEWARE = [
