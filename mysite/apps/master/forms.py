5d8c8c9b3c79ff35ad7a6b51148f6a759aabafac
FarhanGits <daffafarhan05@gmail.com>
2023-11-03 15:17:07 +0700

    koreksi dikit lagi

--- mysite/apps/master/forms.py
+++ mysite/apps/master/forms.py
@@ -1,15 +1,11 @@
-# from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 from .models import Editor
 
 
 class EditorForm(forms.Form):
-    email = forms.EmailField(
-        widget=forms.EmailInput(attrs={"placeholder": "E-Mail.."}), label="E-Mail"
-    )
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "EMail"}))
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"placeholder": "Password.."}),
-        label="Password",
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
     )
 
     class Meta:
