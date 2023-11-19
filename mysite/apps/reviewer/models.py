9a1486a5e043e4d6cc20937b768b737ae95dfe19
ekaprasetiya31 <ekaprasetiyawhy31@gmail.com>
2023-11-19 20:03:25 +0700

    edit-reviewer-data

--- mysite/apps/reviewer/models.py
+++ mysite/apps/reviewer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Reviewer(models.Model):
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    institution = models.CharField(max_length=100)
+    scopus_id = models.CharField(max_length=10)
+    scholar_id = models.CharField(max_length=100)
+    
+    class Meta:  
+        db_table = "reviewer"
--- mysite/apps/reviewer/views.py
+++ mysite/apps/reviewer/views.py
@@ -1,8 +1,50 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from apps.reviewer.models import Reviewer  
 
-def reviewer_list(request):
-    return render(request, "reviewer/reviewer_list.html")
+# Create your views here.
+def reviewer_list(request):  
+    reviewers = Reviewer.objects.all()  
+    return render(request,"reviewer_list.html",{'reviewers':reviewers})
+
+def add(request):
+    if request.method == 'POST':
+        fullname = request.POST.get('fullname')
+        email = request.POST.get('email')
+        institution = request.POST.get('institution')
+        scopus_id = request.POST.get('scopus_id')
+        scholar_id = request.POST.get('scholar_id')
+
+        reviewer = Reviewer(
+            fullname=fullname,
+            email=email,
+            institution=institution,
+            scopus_id=scopus_id,
+            scholar_id=scholar_id
+        )
+
+        reviewer.save()
+        return redirect('/') 
+
+def edit(request, id):
+    reviewer = Reviewer.objects.get(id=id)
+
+    if request.method == 'POST':
+        reviewer.fullname = request.POST.get('fullname')
+        reviewer.email = request.POST.get('email')
+        reviewer.institution = request.POST.get('institution')
+        reviewer.scopus_id = request.POST.get('scopus_id')
+        reviewer.scholar_id = request.POST.get('scholar_id')
+
+        reviewer.save()
+
+        return redirect('/')
+
+
+def delete(request, id):
+    employee = Reviewer.objects.get(id=id)  
+    employee.delete()  
+    return redirect("/") 
 
 def submission(request):
     return render(request, "reviewer/index.html")
