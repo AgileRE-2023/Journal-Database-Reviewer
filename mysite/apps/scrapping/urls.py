ff2a05380d44e5bb2e14946f73504808252f2996
PrjMhmmd <prajam30@gmail.com>
2023-11-25 23:14:04 +0700

    menambahkan url scraping-jurnal

--- mysite/apps/scrapping/urls.py
+++ mysite/apps/scrapping/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 # ... your other URL patterns ...
 
 
 urlpatterns = [
+    path("scraping-jurnal/", views.scraping_jurnal, name="scraping_jurnal"),
 ]
