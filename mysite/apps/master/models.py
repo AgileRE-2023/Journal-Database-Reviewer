5bed9a9ac93b0e98dd4a17ae943dab40dae1f187
rrtxt <rizfiferdian@gmail.com>
2023-11-26 11:59:42 +0700

    Add sync func to fetch from Scopus API

--- mysite/apps/master/models.py
+++ mysite/apps/master/models.py
@@ -83,3 +83,4 @@ class Reviewer(models.Model):
     
     
     
+    
--- mysite/apps/scrapping/views.py
+++ mysite/apps/scrapping/views.py
@@ -1,10 +1,12 @@
+from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect
 from elsapy.elsclient import ElsClient
 from elsapy.elssearch import ElsSearch
 from apps.master.models import Reviewer
 
 import json
+from apps.master.models import Reviewer
 
 # Create your views here.
 file = open('config.json')
@@ -13,23 +15,30 @@ config = json.load(file)
 
 client = ElsClient(api_key=config['apikey'])
 
-def reviewerScrapping(request):
+async def reviewerScrapping(request):
     reviewers = Reviewer.objects.filter(scopus_id__isnull=True)[0:10]
     
-    res = []
+    response = await searchAuthorData(reviewers)
+    messages.success(request, f"Updated {response} Reviewer Scopus ID")
     
-    for reviewer in reviewers:
+    return redirect('dashboard') 
+
+async def searchAuthorData(reviewers : list[Reviewer]):
+    success = 0
+    async for reviewer in reviewers:
         full_name = reviewer.name.split()
         first_name = full_name[0]
         last_name = ' '.join(full_name[1: len(full_name)])
         doc_srch = ElsSearch(f"authfirst({first_name}) and authlast({last_name})", 'author')
         doc_srch.execute(client, get_all=False)
-        
         if doc_srch.results and doc_srch.results[0].get('error') is not None:
-            res.append('error') 
+            scopus_id = None 
         else:
             identifier = doc_srch.results[0]['dc:identifier']
             scopus_id = identifier.split(':')[1]
-            res.append(scopus_id)
+            success += 1
         
-    return HttpResponse(res)
+        reviewer.scopus_id = scopus_id
+        await reviewer.asave()
+    
+    return success
