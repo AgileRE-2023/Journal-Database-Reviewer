ccb09e7a9159c4216119170c658194090e2f654a
rrtxt <rizfiferdian1@gmail.com>
2024-06-27 15:46:34 +0700

    Add date error handling in dashboard

--- mysite/apps/editor/views.py
+++ mysite/apps/editor/views.py
@@ -4,8 +4,12 @@ from apps.reviewer.views import upload
 
 # Create your views here.
 def dashboard(request):
-  reviewer_scrap_date = Scrapping.objects.filter(isReviewerScrap = True).latest('scrapping_date')
-  journal_scrap_date = Scrapping.objects.filter(isReviewerScrap = False).latest('scrapping_date')
-  upload_data_date = Upload_Data.objects.latest("upload_date")
-  context = {"reviewer" : reviewer_scrap_date, "journal" : journal_scrap_date, "upload" : upload_data_date}
+  try:
+    reviewer_scrap_date = Scrapping.objects.filter(isReviewerScrap = True).latest('scrapping_date')
+    journal_scrap_date = Scrapping.objects.filter(isReviewerScrap = False).latest('scrapping_date')
+    upload_data_date = Upload_Data.objects.latest("upload_date")
+    context = {"reviewer" : reviewer_scrap_date, "journal" : journal_scrap_date, "upload" : upload_data_date}
+  except:
+    context = {"reviewer" : None, "journal" : None, "upload" : None}
+    
   return render(request, 'editor/dashboard.html', context)
